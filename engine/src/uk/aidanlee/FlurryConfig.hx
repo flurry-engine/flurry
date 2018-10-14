b90ef9375f943a5be09eadc50fb1c16a00605040
Aidan <aidan.lee63@gmail.com>
2018-10-14 13:32:54 +0100

    Added Flurry class and config class, samples now use them

---
+++ engine/src/uk/aidanlee/FlurryConfig.hx
@@ -0,0 +1,138 @@
+package uk.aidanlee;
+
+import uk.aidanlee.resources.Parcel.ParcelList;
+import uk.aidanlee.gpu.Renderer.RequestedBackend;
+
+class FlurryConfig
+{
+    /**
+     * All the window config options.
+     */
+    public final window : FlurryWindowConfig;
+
+    /**
+     * All the renderer config options.
+     */
+    public final renderer : FlurryRendererConfig;
+
+    /**
+     * All the resource config options.
+     */
+    public final resources : FlurryResourceConfig;
+
+    public function new()
+    {
+        window    = new FlurryWindowConfig();
+        renderer  = new FlurryRendererConfig();
+        resources = new FlurryResourceConfig();
+    }
+}
+
+private class FlurryWindowConfig
+{
+    /**
+     * If the window should be launched in fullscreen borderless mode. (Defaults false)
+     */
+    public var fullscreen : Bool;
+
+    /**
+     * If the window is resiable by the user. (Defaults true)
+     */
+    public var resizable : Bool;
+
+    /**
+     * If the window should be borderless. (Defaults false)
+     */
+    public var borderless : Bool;
+
+    /**
+     * The initial width of the window.
+     */
+    public var width : Int;
+
+    /**
+     * The initial height of the window.
+     */
+    public var height : Int;
+
+    /**
+     * The title of the window.
+     */
+    public var title : String;
+
+    /**
+     * Create a window config class with the default settings.
+     */
+    public function new()
+    {
+        fullscreen = false;
+        resizable  = true;
+        borderless = false;
+        width      = 1280;
+        height     = 720;
+        title      = "Flurry";
+    }
+}
+
+private class FlurryRendererConfig
+{
+    /**
+     * Force the renderer to use a specific backend.
+     * If left unchanged it will attempt to auto-select the best backend for the platform.
+     */
+    public var backend : RequestedBackend;
+
+    /**
+     * The maximum number of vertices allowed in the dynamic vertex buffer. (Defaults 1000000)
+     */
+    public var dynamicVertices : Int;
+
+    /**
+     * The maximum number of vertices allowed in the unchanging vertex buffer. (Defaults 100000)
+     */
+    public var unchangingVertices : Int;
+
+    /**
+     * The default clear colour used by the renderer.
+     */
+    public final clearColour : { r : Float, g : Float, b : Float, a : Float };
+
+    /**
+     * Creates a new renderer config with the default settings.
+     */
+    public function new()
+    {
+        dynamicVertices    = 1000000;
+        unchangingVertices = 100000;
+        clearColour        = { r : 0.2, g : 0.2, b : 0.2, a : 1.0 };
+    }
+}
+
+private class FlurryResourceConfig
+{
+    /**
+     * If the standard shader parcel should not be loaded. (Defaults true).
+     */
+    public var includeStdShaders : Bool;
+
+    /**
+     * Any resources placed into this parcel list will be loaded before the Flurry's onReady function is called.
+     */
+    public final preload : ParcelList;
+
+    /**
+     * Create a new resource config with the default settings.
+     */
+    public function new()
+    {
+        includeStdShaders = true;
+        preload = {
+            bytes   : [],
+            texts   : [],
+            jsons   : [],
+            images  : [],
+            shaders : [],
+            parcels : []
+        };
+    }
+}
--- sample/scenes/src/Main.hx
+++ sample/scenes/src/Main.hx
@@ -1,289 +1,43 @@
 package;
 
-import snow.App;
-import snow.types.Types;
-import hxtelemetry.HxTelemetry;
-import uk.aidanlee.gpu.Renderer;
-import uk.aidanlee.gpu.imgui.ImGuiImpl;
-import uk.aidanlee.resources.ResourceSystem;
-import uk.aidanlee.resources.Resource;
+import uk.aidanlee.Flurry;
+import uk.aidanlee.FlurryConfig;
 import uk.aidanlee.scene.Scene;
 import imgui.ImGui;
 import imgui.util.ImVec2;
 
 typedef UserConfig = {};
 
-class Main extends App
+class Main extends Flurry
 {
-    /**
-     * Haxe telemetry instance. Useful for profiling the game.
-     * TODO : ifdef this behind some compile time flag so release builds won't include it.
-     */
-    var telemetry : HxTelemetry;
-
-    /**
-     * Games renderer.
-     */
-    var renderer : Renderer;
-
-    /**
-     * Games main resource system.
-     * Users can create more of them if they wish but are responsible for managing them themselves.
-     */
-    var resources : ResourceSystem;
-
-    /**
-     * ImGui implementation. Allows for quick debug menus on native targets.
-     * TODO : ifdef imgui stuff behind a cpp check, or finally get around to creating emscripten bindings for imgui and a wrangler lib.
-     */
-    var imgui : ImGuiImpl;
-
-    /**
-     * If this engines default assets have loaded.
-     */
-    var loaded : Bool;
-
-    /**
-     * Root scene node.
-     */
-    var root : Scene;
-
-    public function new() {}
-
-    override function config(_config : AppConfig) : AppConfig
+    override function onConfig(_config : FlurryConfig) : FlurryConfig
     {
-        _config.window.title            = 'gpu';
-        _config.window.width            = 1600;
-        _config.window.height           = 900;
-        _config.window.background_sleep = null;
+        _config.window.title  = 'Flurry';
+        _config.window.width  = 1600;
+        _config.window.height = 900;
 
-        // Core 3.2 is the minimum required version.
-        // Many graphics stacks don't support compatibility profiles and core 3.2 is supported pretty much everywhere by now.
-        _config.render.opengl.major = 3;
-        _config.render.opengl.minor = 2;
-        _config.render.opengl.profile = core;
+        _config.renderer.backend = GL45;
+
+        _config.resources.preload.parcels.push('assets/parcels/sample.parcel');
 
         return _config;
     }
 
-    /**
-     * Once snow is ready we can create our engine instances and load a parcel with some default assets.
-     */
-    override function ready()
+    override function onReady()
     {
-        loaded    = false;
-        telemetry = new HxTelemetry();
-
-        // Setup snow timestep.
-        // Fixed dt of 16.66
-        fixed_timestep = true;
-        update_rate    = 1 / 60;
-
-        // Disable auto swapping. We will swap ourselves if the renderer backend requires it.
-        app.runtime.auto_swap = false;
-        
-        // Setup the renderer.
-        renderer = new Renderer({
-
-            // The api you choose changes what shaders you need to provide
-            // Possible APIs are WEBGL, GL45, DX11, and NULL
-            api    : WEBGL,
-            width  : app.runtime.window_width(),
-            height : app.runtime.window_height(),
-            dpi    : app.runtime.window_device_pixel_ratio(),
-            maxUnchangingVertices :  100000,
-            maxDynamicVertices    : 1000000,
-            backend : {
-
-                // This tells the GL4.5 backend if we can use bindless textures
-                bindless : sdl.SDL.GL_ExtensionSupported('GL_ARB_bindless_texture'),
-
-                // The DX11 backend needs to know the games window so it can fetch the HWND for the DXGI swapchain.
-                window : app.runtime.window
-            }
-        });
-
-        // Pass the renderer backend to the resource system so GPU resources (textures, shaders) can be automatically managed.
-        // When loading and freeing parcels the needed GPU resources can then be created and destroyed as and when needed.
-        resources = new ResourceSystem(renderer.backend);
-
-        // Load a pre-packed parcel containing our shader and two images.
-        // See the parcel tool for creating pre-packed parcels.
-        resources.createParcel('default', { parcels : [ 'assets/parcels/sample.parcel' ] }, onLoaded).load();
-
-        // Setup a default root scene, in the future users will specify their root scene.
         root = createTree();
         root.resumeOnCreation = true;
-    }
-
-    /**
-     * Simulate all of the engines components.
-     * @param _dt 
-     */
-    override function update(_dt : Float)
-    {
-        // The resource system needs to be called periodically to process thread events.
-        // If this is not called the resources loaded on separate threads won't be registered and parcel callbacks won't be invoked.
-        resources.update();
-
-        // Pre-draw
-        renderer.clear();
-        renderer.preRender();
-
-        // Our game specific logic, only do it if our default parcel has loaded.
-        if (loaded)
-        {
-            imgui.newFrame();
-
-            root.update(_dt);
-        }
-
-        // Render and present
-        renderer.render();
-
-        if (loaded)
-        {
-            uiShowRenderStats();
-            imgui.render();
-        }
-
-        // Post-draw
-        // The window_swap is only needed for GL renderers with snow.
-        renderer.postRender();
-        if (renderer.api != DX11)
-        {
-            app.runtime.window_swap();
-        }
-
-        telemetry.advance_frame();
-    }
-
-    /**
-     * On shutdown remove the default parcel to free resources.
-     */
-    override function ondestroy()
-    {
-        root.remove();
-
-        imgui.dispose();
-
-        resources.free('default');
-    }
-
-    // #region event functions.
-
-    override function onevent(_event : SystemEvent)
-    {
-        if (_event.window != null)
-        {
-            if (_event.window.type == WindowEventType.we_resized)
-            {
-                renderer.resize(_event.window.x, _event.window.y);
-            }
-        }
-    }
-
-    override function onkeyup(_keycode : Int, _scancode : Int, _repeat : Bool, _mod : ModState, _timestamp : Float, windowID : Int)
-    {
-        if (!loaded) return;
-
-        root.keyUp(_keycode, _scancode, _repeat, _mod);
-    }
-
-    override function onkeydown(_keycode : Int, _scancode : Int, _repeat : Bool, _mod : ModState, _timestamp : Float, windowID : Int)
-    {
-        if (!loaded) return;
-
-        if (_keycode == Key.space)
-        {
-            var found = root.getChild('root/child1/child2/child1');
-            trace(found);
-        }
-
-        root.keyDown(_keycode, _scancode, _repeat, _mod);
-    }
-
-    override function ontextinput(_text : String, _start : Int, _length : Int, _type : TextEventType, _timestamp : Float, _windowID : Int)
-    {
-        if (!loaded) return;
-
-        root.textInput(_text, _start, _length, _type);
-
-        imgui.onTextInput(_text);
-    }
-
-    override function onmouseup(_x : Int, _y : Int, _button : Int, _timestamp : Float, _windowID : Int)
-    {
-        if (!loaded) return;
-
-        root.mouseUp(_x, _y, _button);
-    }
-
-    override function onmousedown(_x : Int, _y : Int, _button : Int, _timestamp : Float, _windowID : Int)
-    {
-        if (!loaded) return;
-
-        root.mouseDown(_x, _y, _button);
-    }
-
-    override function onmousemove(_x : Int, _y : Int, _xRel : Int, _yRel : Int, _timestamp : Float, _windowID : Int)
-    {
-        if (!loaded) return;
-
-        root.mouseMove(_x, _y, _xRel, _yRel);
-
-        imgui.onMouseMove(_x, _y);
-    }
-
-    override function onmousewheel(_x : Float, _y : Float, _timestamp : Float, _windowID : Int)
-    {
-        if (!loaded) return;
-
-        root.mouseWheel(_x, _y);
-
-        imgui.onMouseWheel(_y);
-    }
-
-    override function ongamepadup(_gamepad : Int, _button : Int, _value : Float, _timestamp : Float)
-    {
-        if (!loaded) return;
-
-        root.gamepadUp(_gamepad, _button, _value);
-    }
-
-    override function ongamepaddown(_gamepad : Int, _button : Int, _value : Float, _timestamp : Float)
-    {
-        if (!loaded) return;
-
-        root.gamepadDown(_gamepad, _button, _value);
-    }
-
-    override function ongamepadaxis(_gamepad : Int, _axis : Int, _value : Float, _timestamp : Float)
-    {
-        if (!loaded) return;
-
-        root.gamepadAxis(_gamepad, _axis, _value);
-    }
-
-    override function ongamepaddevice(_gamepad : Int, _id : String, _type : GamepadDeviceEventType, _timestamp : Float)
-    {
-        if (!loaded) return;
-        
-        root.gamepadDevice(_gamepad, _id, _type);
-    }
-
-    // #endregion
-
-    /**
-     * Once the default assets have been loaded create our imgui helper and kick start our root scene.
-     * @param _resources Resources loaded.
-     */
-    function onLoaded(_resources : Array<Resource>)
-    {
-        imgui  = new ImGuiImpl(app, cast renderer.backend, resources.get('assets/shaders/textured.json', ShaderResource));
-        loaded = true;
-
         root.create();
+    }
+
+    override function onUpdate(_dt : Float)
+    {
+        root.update(_dt);
+    }
+
+    override function onPostUpdate()
+    {
+        uiShowRenderStats();
     }
 
     /**
@@ -326,7 +80,7 @@ class Main extends App
      */
     function createTree() : Scene
     {
-        var rootNode = new Scene('root', app, null, renderer, resources, null);
+        var rootNode = new Scene('root', app, null, renderer, resources, events);
         var child1   = rootNode.addChild(Scene, 'root/child1');
         rootNode.addChild(Scene, 'root/child2');
         rootNode.addChild(Scene, 'root/child3');
