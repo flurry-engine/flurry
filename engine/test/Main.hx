425570735e0fa4f31eabdde54a42a352c482e8e7
Aidan <aidan.lee63@gmail.com>
2018-12-08 22:01:18 +0000

    Updated test suite to not use some messy snow based testing nonsense

--- engine/test/Main.hx
+++ engine/test/Main.hx
@@ -2,45 +2,34 @@ package;
 
 import buddy.*;
 
-typedef UserConfig = {};
+class Main implements Buddy<[
+    tests.api.maths.MathsTests,
+    tests.api.maths.VectorTests,
+    tests.api.maths.QuaternionTests,
+    tests.api.maths.MatrixTests,
+    tests.api.maths.RectangleTests,
+    tests.api.maths.CircleTests,
 
-class Main extends snow.App
+    tests.api.importers.bmfont.BitmapFontParserTests,
+    tests.api.importers.textureatlas.TextureAtlasParserTests
+    
+    /*
+    new tests.api.gpu.batcher.BatcherTests(),
+    new tests.api.gpu.geometry.VertexTests(),
+    new tests.api.gpu.geometry.GeometryTests(),
+    new tests.api.gpu.geometry.ColorTests(),
+    new tests.api.gpu.geometry.shapes.ArcGeometryTests(),
+    new tests.api.gpu.geometry.shapes.LineGeometryTests(),
+    new tests.api.gpu.geometry.shapes.RingGeometryTests(),
+    new tests.api.gpu.geometry.shapes.QuadGeometryTests(),
+    new tests.api.gpu.geometry.shapes.TextGeometryTests(),
+    new tests.api.gpu.geometry.shapes.CircleGeometryTests(),
+    new tests.api.gpu.geometry.shapes.QuadPackGeometryTests(),
+    new tests.api.gpu.geometry.shapes.RectangleGeometryTests(),
+
+    new tests.modules.scene.SceneTests()
+    */
+]>
 {
-    public function new() {}
-
-    override function ready()
-    {
-        var runner = new SuitesRunner([
-            new tests.api.maths.MathsTests(),
-            new tests.api.maths.VectorTests(),
-            new tests.api.maths.QuaternionTests(),
-            new tests.api.maths.MatrixTests(),
-            new tests.api.maths.RectangleTests(),
-            new tests.api.maths.CircleTests(),
-            
-            new tests.api.importers.bmfont.BitmapFontParserTests(),
-            new tests.api.importers.textureatlas.TextureAtlasParserTests(),
-
-            /*
-            new tests.api.gpu.batcher.BatcherTests(),
-            new tests.api.gpu.geometry.VertexTests(),
-            new tests.api.gpu.geometry.GeometryTests(),
-            new tests.api.gpu.geometry.ColorTests(),
-            new tests.api.gpu.geometry.shapes.ArcGeometryTests(),
-            new tests.api.gpu.geometry.shapes.LineGeometryTests(),
-            new tests.api.gpu.geometry.shapes.RingGeometryTests(),
-            new tests.api.gpu.geometry.shapes.QuadGeometryTests(),
-            new tests.api.gpu.geometry.shapes.TextGeometryTests(),
-            new tests.api.gpu.geometry.shapes.CircleGeometryTests(),
-            new tests.api.gpu.geometry.shapes.QuadPackGeometryTests(),
-            new tests.api.gpu.geometry.shapes.RectangleGeometryTests(),
-
-            new tests.modules.scene.SceneTests()
-            */
-        ], new ColorReporter());
-
-        runner.run();
-
-        app.shutdown();
-    }
+    //
 }
