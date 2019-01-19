f589961dcc5cde65b318f97d45a72a0b0225bfd9
Aidan <aidan.lee63@gmail.com>
2019-01-19 18:29:55 +0000

    Simplified ci install scripts and test setup

---
+++ ci/test_engine.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd engine/test
+
+haxe build.hxml -D report-name=Engine-Tests-$AGENT_OS
+
+if [ $AGENT_OS == 'Windows_NT' ]; then
+    ./bin/cpp/Main.exe
+else
+    ./bin/cpp/Main
+fi
