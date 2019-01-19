a3b6367996acf9f5103f1585dae376fa1db1d0a6
Aidan <aidan.lee63@gmail.com>
2019-01-19 17:48:17 +0000

    Download and use haxe nightly

--- ci/install_lix.sh
+++ ci/install_lix.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-node --version
-
 # Install linux specific dependencies
 if [ $AGENT_OS == 'Linux' ]; then
     sudo apt-get install -y gcc-multilib g++-multilib libgl1-mesa-dev libglu1-mesa-dev libopenal-dev libxrandr-dev libxinerama-dev libasound2-dev libsdl2-dev
 fi
 
 # Install lix and download flurry dependencies
-sudo npm install git+https://git@github.com/starburst997/lix.client.git --global
+npm install git+https://git@github.com/starburst997/lix.client.git --global
 
 lix download
+lix install haxe nightly
+lix use haxe nightly
 
 # Build the hxcpp tools
 cd $(haxelib path hxcpp | tail -1 | tr -d '\n')/tools/run
