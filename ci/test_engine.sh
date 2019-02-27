d8ef3c563f8066b5778509bc0ba137b75beae23a
Aidan <aidan.lee63@gmail.com>
2019-02-27 20:07:57 +0000

    flurry repo re-factor

--- ci/test_engine.sh
+++ ci/test_engine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd engine/test
+cd test
 
 haxe build.hxml -D reporter=XUnit2Reporter -D report-name=Engine-Tests-$AGENT_OS
 
---
+++ test/src/tests/modules/differ/data/RayCollisionTests.hx
@@ -0,0 +1,16 @@
+package tests.modules.differ.data;
+
+import uk.aidanlee.flurry.modules.differ.data.RayCollision;
+import buddy.BuddySuite;
+
+using buddy.Should;
+
+class RayCollisionTests extends BuddySuite
+{
+    public function new()
+    {
+        describe('RayCollision', {
+            //
+        });
+    }
+}
