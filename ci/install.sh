c02bc546cb3fafd586f68c617bd03e40c9eb8ecd
Aidan Lee <aidan.lee63@gmail.com>
2019-06-01 18:10:37 +0100

    update lix client fork

--- ci/install.sh
+++ ci/install.sh
@@ -6,7 +6,7 @@ if [ $AGENT_OS == 'Linux' ]; then
 fi
 
 # Install lix and download flurry dependencies
-npm install git+https://git@github.com/starburst997/lix.client.git --global
+npm install git+https://git@github.com/aidan63/lix.client.git#git-flat-flag --global
 
 lix download
 
