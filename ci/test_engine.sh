2c7ab66f6675598415267f04f754097f8cf9ed80
Aidan Lee <aidan.lee63@gmail.com>
2019-11-30 16:48:04 +0000

    Fix mis-spelt cli argument

--- ci/test_engine.sh
+++ ci/test_engine.sh
@@ -2,7 +2,7 @@
 
 cd tests/unit
 
-npx haxe build.hxml -D reporter=XUnit2Reporter -D report-name=Engine-Tests-$AGENT_OS --debug --no-trace
+npx haxe build.hxml -D reporter=XUnit2Reporter -D report-name=Engine-Tests-$AGENT_OS --debug --no-traces
 
 if [ $AGENT_OS == 'Windows_NT' ]; then
     ./bin/Main-debug.exe
