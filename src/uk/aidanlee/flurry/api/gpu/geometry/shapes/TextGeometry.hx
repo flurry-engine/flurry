9674627765c26eb61f97dd6f2b87e56f9fb173ba
Aidan63 <aidan.lee63@gmail.com>
2020-02-08 18:40:27 +0000

    Fix text geometry not rendering

--- src/uk/aidanlee/flurry/api/gpu/geometry/shapes/TextGeometry.hx
+++ src/uk/aidanlee/flurry/api/gpu/geometry/shapes/TextGeometry.hx
@@ -1,5 +1,7 @@
 package uk.aidanlee.flurry.api.gpu.geometry.shapes;
 
+import uk.aidanlee.flurry.api.buffers.UInt16BufferData;
+import uk.aidanlee.flurry.api.buffers.Float32BufferData;
 import uk.aidanlee.flurry.api.gpu.geometry.VertexBlob.VertexBlobBuilder;
 import uk.aidanlee.flurry.api.importers.bmfont.BitmapFontData;
 import uk.aidanlee.flurry.api.gpu.geometry.Geometry;
@@ -96,40 +98,66 @@ class TextGeometry extends Geometry
      */
     function generateGeometry()
     {
-        // // Remove all verticies.
-        // vertices.resize(0);
+        final lines = text.split('\n');
 
-        // // Generate all of the text geometry.
-        // for (line in text.split('\n'))
-        // {
-        //     for (i in 0...line.length)
-        //     {
-        //         var char = font.chars.get(line.charCodeAt(i));
+        var count = 0;
+        for (line in lines)
+        {
+            count += line.length;
+        }
 
-        //         // Move the cursor by the kerning amount.
-        //         if (i != 0)
-        //         {
-        //             var map = font.kernings.get(char.id);
-        //             if (map != null)
-        //             {
-        //                 var value = map.get(line.charCodeAt(i - 1));
-        //                 if (value != null)
-        //                 {
-        //                     cursorPosition.x += value;
-        //                 }
-        //             }
-        //         }
+        final vtxBuffer = new Float32BufferData(count * 4 * 9);
+        final idxBuffer = new UInt16BufferData(count * 6);
+        var vtxOffset = 0;
+        var idxOffset = 0;
+        var baseIndex = 0;
 
-        //         // Add the character quad.
-        //         addCharacter(char, cursorPosition.x, cursorPosition.y);
+        var texWidth  = 1;
+        var texHeight = 1;
+        switch textures
+        {
+            case Textures(_textures):
+                texWidth  = _textures[0].width;
+                texHeight = _textures[0].height;
+            case _:
+        }
 
-        //         // Move the cursor to the next characters position.
-        //         cursorPosition.x += char.xAdvance;
-        //     }
+        for (line in lines)
+        {
+            for (i in 0...line.length)
+            {
+                final char = font.chars.get(line.charCodeAt(i));
 
-        //     cursorPosition.y += font.lineHeight;
-        //     cursorPosition.x  = position.x;
-        // }
+                // Move the cursor by the kerning amount.
+                if (i != 0)
+                {
+                    if (font.kernings.exists(char.id))
+                    {
+                        final map   = font.kernings.get(char.id);
+                        final value = map.get(line.charCodeAt(i - 1));
+
+                        if (value != null)
+                        {
+                            cursorPosition.x += value;
+                        }
+                    }
+                }
+
+                // Add the character quad.
+                addCharacter(vtxBuffer, idxBuffer, vtxOffset, idxOffset, baseIndex, char, cursorPosition.x, cursorPosition.y, texWidth, texHeight);
+                vtxOffset += 4 * 9;
+                idxOffset += 6;
+                baseIndex += 4;
+
+                // Move the cursor to the next characters position.
+                cursorPosition.x += char.xAdvance;
+            }
+
+            cursorPosition.y += font.lineHeight;
+            cursorPosition.x  = position.x;
+        }
+
+        data = Indexed(new VertexBlob(vtxBuffer), new IndexBlob(idxBuffer));
     }
 
     /**
@@ -138,24 +166,68 @@ class TextGeometry extends Geometry
      * @param _x Top left start x for the quad.
      * @param _y Top left start y for the quad.
      */
-    function addCharacter(_char : Character, _x : Float, _y : Float)
+    function addCharacter(
+        _vtxBuffer : Float32BufferData,
+        _idxBuffer : UInt16BufferData,
+        _vtxOffset : Int,
+        _idxOffset : Int,
+        _baseIndex : Int,
+        _char : Character,
+        _x : Float,
+        _y : Float,
+        _texWidth : Float,
+        _texHeight : Float)
     {
-        // var tlPos = new Vector3((_x + _char.xOffset)              , _y + _char.yOffset);
-        // var trPos = new Vector3((_x + _char.xOffset) + _char.width, _y + _char.yOffset);
-        // var blPos = new Vector3((_x + _char.xOffset)              , _y + _char.yOffset + _char.height);
-        // var brPos = new Vector3((_x + _char.xOffset) + _char.width, _y + _char.yOffset + _char.height);
+        // bottom left
+        _vtxBuffer[_vtxOffset++] = _x + _char.xOffset;
+        _vtxBuffer[_vtxOffset++] = _y + _char.yOffset + _char.height;
+        _vtxBuffer[_vtxOffset++] = 0;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = _char.x / _texWidth;
+        _vtxBuffer[_vtxOffset++] = (_char.y + _char.height) / _texHeight;
 
-        // var tlUV = new Vector2(_char.x / textures[0].width                , _char.y / textures[0].height);
-        // var trUV = new Vector2((_char.x + _char.width) / textures[0].width, _char.y / textures[0].height);
-        // var blUV = new Vector2(_char.x / textures[0].width                , (_char.y + _char.height) / textures[0].height);
-        // var brUV = new Vector2((_char.x + _char.width) / textures[0].width, (_char.y + _char.height) / textures[0].height);
+        // Bottom right
+        _vtxBuffer[_vtxOffset++] = _x + _char.xOffset + _char.width;
+        _vtxBuffer[_vtxOffset++] = _y + _char.yOffset + _char.height;
+        _vtxBuffer[_vtxOffset++] = 0;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = (_char.x + _char.width) / _texWidth;
+        _vtxBuffer[_vtxOffset++] = (_char.y + _char.height) / _texHeight;
 
-        // vertices.push(new Vertex( blPos, color, blUV ));
-        // vertices.push(new Vertex( brPos, color, brUV ));
-        // vertices.push(new Vertex( tlPos, color, tlUV ));
+        // Top left
+        _vtxBuffer[_vtxOffset++] = _x + _char.xOffset;
+        _vtxBuffer[_vtxOffset++] = _y + _char.yOffset;
+        _vtxBuffer[_vtxOffset++] = 0;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = _char.x / _texWidth;
+        _vtxBuffer[_vtxOffset++] = _char.y / _texHeight;
 
-        // vertices.push(new Vertex( tlPos, color, tlUV ));
-        // vertices.push(new Vertex( brPos, color, brUV ));
-        // vertices.push(new Vertex( trPos, color, trUV ));
+        // Top right
+        _vtxBuffer[_vtxOffset++] = (_x + _char.xOffset) + _char.width;
+        _vtxBuffer[_vtxOffset++] = _y + _char.yOffset;
+        _vtxBuffer[_vtxOffset++] = 0;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = 1;
+        _vtxBuffer[_vtxOffset++] = (_char.x + _char.width) / _texWidth;
+        _vtxBuffer[_vtxOffset++] = _char.y / _texHeight;
+
+        // indicies
+        _idxBuffer[_idxOffset++] = _baseIndex + 0;
+        _idxBuffer[_idxOffset++] = _baseIndex + 1;
+        _idxBuffer[_idxOffset++] = _baseIndex + 2;
+        _idxBuffer[_idxOffset++] = _baseIndex + 2;
+        _idxBuffer[_idxOffset++] = _baseIndex + 1;
+        _idxBuffer[_idxOffset++] = _baseIndex + 3;
     }
 }
