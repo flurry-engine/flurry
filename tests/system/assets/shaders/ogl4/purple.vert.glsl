fbc45c2b5557148b34ba311a6067f39aa23e7154
Aidan63 <aidan.lee63@gmail.com>
2020-08-14 14:33:15 +0100

    get rid of ogl3 and hlsl shaders in the system test assets directory

--- tests/system/assets/shaders/ogl4/purple.vert.glsl
+++ tests/system/assets/shaders/ogl4/purple.vert.glsl
@@ -11,13 +11,6 @@ layout(std140, binding = 0) uniform flurry_matrices
     mat4 model;
 };
 
-layout(std140, binding = 1) uniform colours
-{
-    float red;
-    float green;
-    float blue;
-};
-
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
