d070bb77490e78419cc6a9dea7845847c0a11742
Aidan63 <aidan.lee63@gmail.com>
2021-08-29 19:51:21 +0100

    Add world to screen and screen to world function to camera2d

--- src/lib/uk/aidanlee/flurry/api/gpu/camera/Camera2D.hx
+++ src/lib/uk/aidanlee/flurry/api/gpu/camera/Camera2D.hx
@@ -1,6 +1,7 @@
 package uk.aidanlee.flurry.api.gpu.camera;
 
 import VectorMath;
+import uk.aidanlee.flurry.api.maths.Matrix;
 
 class Camera2D
 {
@@ -26,13 +27,13 @@ class Camera2D
         angle    = 0;
     }
 
-    public inline function worldToScreen(_vec : Vec2)
+    public inline function worldToScreen(_world : Vec2)
     {
-        return vec2(0);
+        return vec2(vec4(_world, 0, 1) * inverse(make2D(pos, origin, scale, angle)));
     }
 
-    public inline function screenToWorld(_vec : Vec2)
+    public inline function screenToWorld(_screen : Vec2)
     {
-        return vec2(0);
+        return vec2(vec4(_screen, 0, 1) * make2D(pos, origin, scale, angle));
     }
 }
